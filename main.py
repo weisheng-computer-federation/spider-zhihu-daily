fcc56940234b7af64df8ac5c53dd3d80687b893f
Zyyans <1980797568@qq.com>
2019-05-22 20:07:36 +0800

    规范引用

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import datetime, re, requests, os
+from datetime import datetime
+from os import makedirs
+from os.path import exists
+from re import findall
+
 from bs4 import BeautifulSoup
+from requests import get
+
 
 def get_one_page(url):
     # 获取单个页面的信息
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    response = requests.get(url=url, headers=headers)
+    response = get(url=url, headers=headers)
     if response.ok:
         return response
 
@@ -12,7 +18,7 @@ def get_urls():
     # 获取知乎日报每篇文章的url
     main_url = 'https://daily.zhihu.com'
     response = get_one_page(main_url)
-    urls = re.findall('"box".*?href="(.*?)".*?"link-button"', response.text)
+    urls = findall('"box".*?href="(.*?)".*?"link-button"', response.text)
     for iter in range(0, len(urls)):
         urls[iter] = main_url + urls[iter]
     return urls
@@ -25,8 +31,8 @@ def write_one_page(url):
     
     # 创建文件夹
     path = 'zhihu-daily/'
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if not exists(path):
+        makedirs(path)
     path = path + '/' + '知乎日报' + title.replace('/','') + '.md'
     
     try:
@@ -37,7 +43,7 @@ def write_one_page(url):
         image_source = headline.find(name='span').string
         answers = soup.find_all(name='div', attrs={'answer'})
         with open(path, 'w', encoding='UTF-8') as file:
-            file.write('---\ntitle: 知乎日报|$=-titel-=$\ndate: $=-date-=$\ntags: [知乎日报]\n---\n'.replace('$=-titel-=$', title).replace('$=-date-=$', str(datetime.datetime.now())))
+            file.write('---\ntitle: 知乎日报|$=-titel-=$\ndate: $=-date-=$\ntags: [知乎日报]\n---\n'.replace('$=-titel-=$', title).replace('$=-date-=$', str(datetime.now())))
             file.write('# ' + title + '\n')
             file.write(image + '\n')
             if image_source != None:
